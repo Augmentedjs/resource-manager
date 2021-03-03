51d2faa5e3f030c666770484af9143ccbdd41177
thedocbwarren <jedi.master@augmentedjs.com>
2021-03-02 17:31:12 -0800

    adding tests

--- test/resourceManagerSpec.js
+++ test/resourceManagerSpec.js
@@ -10,17 +10,29 @@ class BubbaStrategy extends Strategy {
   };
 };
 
+class NooberStrategy extends Strategy {
+  constructor() {
+    super();
+  };
+
+  doIt() {
+    return "Heya!";
+  };
+};
+
 const STRATEGIES = {
-  "BUBBA": "Bubba"
+  "BUBBA": "Bubba",
+  "NOOBER": "Noober"
 };
 
 const REGISTRY = {
-  "Bubba": BubbaStrategy
+  "Bubba": BubbaStrategy,
+  "Noober": NooberStrategy
 };
 
 class MyManager extends StrategyManager {
-  constructor() {
-    super(STRATEGIES.BUBBA, REGISTRY);
+  constructor(type) {
+    super(type, REGISTRY);
   };
 
   doIt() {
@@ -30,23 +42,46 @@ class MyManager extends StrategyManager {
 
 describe("Given an Resource Manager", () => {
   let s;
-  beforeEach((done) => {
-    s = new MyManager();
-    done();
+
+  describe("can create a Bubba strategy", () => {
+    beforeEach((done) => {
+      s = new MyManager(STRATEGIES.BUBBA);
+      done();
+    });
+
+    afterEach((done) => {
+      s = null;
+      done();
+    });
+
+    it("can init the manager", () => {
+      expect(s).to.not.be.undefined;
+    });
+
+    it("can call the strategy method", () => {
+      const ret = s.doIt();
+      expect(ret).to.equal("Bubba got a big 'ol truck!");
+    });
   });
 
-  afterEach((done) => {
-    s = null;
-    done();
-  });
+  describe("can create a Noober strategy", () => {
+    beforeEach((done) => {
+      s = new MyManager(STRATEGIES.NOOBER);
+      done();
+    });
 
-  it("can init the manager", () => {
-    expect(s).to.not.be.undefined;
-  });
+    afterEach((done) => {
+      s = null;
+      done();
+    });
 
-  it("can call the strategy method", () => {
-    const ret = s.doIt();
-    expect(ret).to.not.be.undefined;
+    it("can init the manager", () => {
+      expect(s).to.not.be.undefined;
+    });
 
+    it("can call the strategy method", () => {
+      const ret = s.doIt();
+      expect(ret).to.equal("Heya!");
+    });
   });
 });
